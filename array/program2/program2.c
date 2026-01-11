ddf5284031b0dffe14fa9e069619a85c251ca8fa
HariHaranio <hariharan.io7331@gmail.com>
2026-01-11 09:37:40 +0530

    program add

--- array/program2/program2.c
+++ array/program2/program2.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// C program to find the sum, largest and smallest number in an array.
 
-#define MAX_SIZE 100
+int main() {
+
+    int Numbers[5], sumArray = 0,i,largeNumber,smallNumber;
+
+    printf("Enter the Number's:");
+
+    for (i = 0;i < 5;i++){
+        scanf("%d",&Numbers[i]);
+    }
+
+    for (i = 0;i < 5;i++){
+        sumArray = sumArray + Numbers[i];
+    }
+
+    largeNumber = Numbers[0];
+    for (i = 1;i < 5;i++){
+        if (largeNumber < Numbers[i]){
+            largeNumber = Numbers[i];
+        }
+    }
+
+
+    smallNumber = Numbers[0];
+    for (i = 1;i < 5;i++){
+        if (smallNumber > Numbers[i]){
+            smallNumber = Numbers[i];
+        }
+    }
+
+    printf("\nthe Sum of the Array: %d\n",sumArray);
+    printf("\nThe large number is: \%d\n",largeNumber);
+    printf("\nThe Small number is: \%d\n",smallNumber);
+    return 0;
+}
