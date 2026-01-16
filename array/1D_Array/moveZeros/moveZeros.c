3f2224edd770bc5bfc3855115ebfebf22262ad0d
HariHaranio <hariharan.io7331@gmail.com>
2026-01-16 10:19:00 +0530

    Completed: code has been completed

--- array/1D_Array/moveZeros/moveZeros.c
+++ array/1D_Array/moveZeros/moveZeros.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-
+// C program to move all zeros to the end of an array
 int main() {
-    int a[10] = {0,1,2,0,3,0,0,8,0,10};
-    int b[10];
+    int a[10] = {0, 1, 0, 3, 12, 0, 5, 0, 7, 0};
+    int i,count = 0;
     int size = sizeof(a) / sizeof(a[0]);
 
-    for (int i = 0;i < size;i++){
-        if(a[i] == 0){
-            b[i] = a[i];
-        }else{
-            b[i] = a[i];
+    for (i=0;i<size;i++){
+        if (a[i] != 0){
+            a[count] = a[i];
+            count++;
         }
+        a[i] = 0;
     }
-
-    for (int i = 0;i < size;i++){
-        printf("%i, ",b[i]);
+    for (i = 0; i < size; i++){
+        printf("%d, ",a[i]);
     }
     return 0;
 }
