3781a128723624c89ef29d62d22d8ec953bd9db4
HariHaranio <hariharan.io7331@gmail.com>
2026-01-29 20:00:14 +0530

    Add: New file

---
+++ array/2D_Array/2Dprogram2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a[2][3] = {{1,2,7},{8,9,6}}, b[2][3] = {{3,8,7},{1,10,4}};
+    int i,j;
+    int rowsA = sizeof(a) / sizeof(a[0]);
+    int colsA = sizeof(a[0]) / sizeof(a[0][0]);
+
+    printf("\nAddition:\n");
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < colsA; j++) {
+            printf("%d ", a[i][j] + b[i][j]);
+        }
+    printf("\n");
+    }
+
+    printf("\nSubtraction:\n");
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < colsA; j++) {
+            printf("%d ", a[i][j] - b[i][j]);
+        }
+    printf("\n");
+    }
+
+    printf("\nMultiplication:\n");
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < colsA; j++) {
+            printf("%d ", a[i][j] * b[i][j]);
+        }
+    printf("\n");
+    }
+
+    printf("\nDivision:\n");
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < colsA; j++) {
+            printf("%d ", a[i][j] / b[i][j]);
+        }
+    printf("\n");
+    }
+    return 0;
+}
