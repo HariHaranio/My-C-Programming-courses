e6472c8c9097733d56bf55d952ef20682778d19b
HariHaranio <hariharan.io7331@gmail.com>
2026-02-24 06:55:45 +0530

    new assigment1

---
+++ Assignnments/assignments1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//this program is to swap two numbers using a third variable
+
+int main(){
+    int a = 5,b = 4;
+    int temp;
+
+    printf("Before swapping a = %d & b = %d\n",a,b);
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("after swapping a = %d & b = %d\n",a,b);
+    return 0;
+}
