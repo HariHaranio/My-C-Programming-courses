b86befc431c1f037ddc5b8573081de3de10ed8df
HariHaranio <hariharan.io7331@gmail.com>
2026-02-24 07:12:52 +0530

    new: assigment

---
+++ Assignnments/assigments5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+// check whether the number is divisible by both  3 and 5.
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    if (num % 3 == 0 && num % 5 == 0) {
+        printf("%d is divisible by both 3 and 5.\n", num);
+    } else {
+        printf("%d is not divisible by both 3 and 5.\n", num);
+    }
+    
+    return 0;
+}
