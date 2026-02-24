3aeee21fd32f992a4ae8456da8e8a16d9d571e6c
HariHaranio <hariharan.io7331@gmail.com>
2026-02-24 06:58:54 +0530

    Add: New file & code

---
+++ Assignnments/assignments2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+// this program is to swap two numbers without using a third variable
+// by using arithmetic operations
+
+
+int main(){
+    int a = 5, b = 4;
+    printf("Before swapping a = %d & b = %d\n",a,b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swapping a = %d & b = %d\n",a,b);
+
+    return 0;
+}
