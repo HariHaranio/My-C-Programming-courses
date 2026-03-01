696ff91b7505953e0aac250a8bafcd8ea50bd5e3
HariHaranio <hariharan.io7331@gmail.com>
2026-03-01 11:40:18 +0530

    New: assignments on 27-02-26

---
+++ Assignments/assignments on (27-02-2026)/program1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+// write a program to check whether a number is postive or negative.
+int main(){
+    int getNum;
+
+    printf("Enter a number to check whether a number is postive or negative:");
+    scanf("%d",&getNum);
+
+    if (getNum > 0){
+        printf("\nGiven number '/ %d '/ is postive",getNum);
+    }else{
+        printf("\nGiven number '/ %d '/ is negative",getNum);
+    }
+    
+    return 0;
+}
