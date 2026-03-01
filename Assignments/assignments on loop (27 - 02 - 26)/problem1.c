5b08b044f26dfbbe68836e925deec616d784267a
HariHaranio <hariharan.io7331@gmail.com>
2026-03-01 12:56:38 +0530

    New: assignments on 27-02-26

---
+++ Assignments/assignments on loop (27 - 02 - 26)/problem1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+// assignment date :27 - 02 - 2026
+// Write a C program to print numbers from 1 to 10 using a loop.
+
+int main(){
+    for (int i = 1;i <= 10;i++){
+        printf("%d\n",i);
+    }
+    return 0;
+}
